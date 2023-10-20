4094d40b04b7af44ab8972d39ea9cd23986643d2
bumbling-bulbasaur <kevine1221@gmail.com>
2023-10-19 23:54:40 -0400

    fixed parallel branch

--- R/mcmc_privacy.R
+++ R/mcmc_privacy.R
@@ -12,13 +12,13 @@
 #'
 #' @examples
 mcmc_privacy <- function(data_model,
-                         sdp,
-                         nobs,
-                         init_par,
-                         niter = 2000,
-                         chains = 1,
-                         warmup = floor(niter / 2),
-                         varnames = NULL) {
+                             sdp,
+                             nobs,
+                             init_par,
+                             niter = 2000,
+                             chains = 1,
+                             warmup = floor(niter / 2),
+                             varnames = NULL) {
   #check inputs
   checkmate::qassert(nobs, "X?(0,)")
   checkmate::qassert(niter, "X?(0,)")
@@ -31,63 +31,61 @@ mcmc_privacy <- function(data_model,
   post_smpl <- data_model$post_smpl
   lik_smpl <- data_model$lik_smpl
   ll_priv_mech <- data_model$ll_priv_mech
-  st_calc <- data_model$st_calc
+  st_init <- data_model$st_calc
+  st_update <- data_model$st_update
   npar <- data_model$npar
 
   accept_mat <- matrix(NA, nrow = niter, ncol = chains)
   theta_clist <- list()
   ret_val <- NULL
   cat("\n")
-  for(k in 1:chains) {
-    print(paste0(cat("\n"), "Chain: ", k, "/", chains, cat("\n")))
-    d_mat <- lapply(1:nobs, function(s) lik_smpl(init_par))
-    d_mat <- do.call(rbind, d_mat)
-    theta_mat <- matrix(0, nrow = niter, ncol = npar)
-    theta <- init_par
-    pb <- utils::txtProgressBar(1, niter * nobs, style=3)
-    st <- st_calc(d_mat)
-    for(i in 1:niter) {
-      counter <- 0
-      theta_mat[i,] <- theta
-      theta <- post_smpl(d_mat, theta)
-      for(j in 1:nobs) {
-        xs <- lik_smpl(theta)
-        xo <- d_mat[j,]
-        sn <- NULL
-        if(!data_model$add) {
-          n_mat <- d_mat
-          n_mat[j,] <- xs
-          sn <- st_calc(n_mat)
-        } else {
-          #sn <- st_update(st, xs, xo)
-          sn <- st - st_calc(t(xo)) + st_calc(t(xs))
+  theta_clist <-
+    foreach(k = 1:chains) %do% {
+      print(paste0(cat("\n"), "Chain: ", k, "/", chains, cat("\n")))
+      d_mat <- lapply(1:nobs, function(s) lik_smpl(init_par))
+      d_mat <- do.call(rbind, d_mat)
+      theta_mat <- matrix(0, nrow = niter, ncol = npar)
+      theta <- init_par
+      pb <- utils::txtProgressBar(1, niter * nobs, style=3)
+      st <- st_init(d_mat)
+      for(i in 1:niter) {
+        counter <- 0
+        theta_mat[i,] <- theta
+        theta <- post_smpl(d_mat, theta)
+        for(j in 1:nobs) {
+          xs <- lik_smpl(theta)
+          xo <- d_mat[j,]
+          sn <- NULL
+          if(is.null(st_update)) {
+            n_mat <- d_mat
+            n_mat[j,] <- xs
+            sn <- st_init(n_mat)
+          } else {
+            sn <- st_update(st, xs, xo)
+          }
+          a <- exp(ll_priv_mech(sdp, sn) - ll_priv_mech(sdp, st))
+          if(stats::runif(1) < min(a,1)) {
+            counter <- counter + 1
+            d_mat[j,] <- xs
+            st <- sn
+          }
+          utils::setTxtProgressBar(pb, i*nobs + j)
         }
-        a <- exp(ll_priv_mech(sdp, sn) - ll_priv_mech(sdp, st))
-        if(stats::runif(1) < min(a,1)) {
-          counter <- counter + 1
-          d_mat[j,] <- xs
-          st <- sn
-        }
-        utils::setTxtProgressBar(pb, i*nobs + j)
+        accept_mat[i, k] <- counter / nobs
       }
-      accept_mat[i, k] <- counter / nobs
+      if(warmup > 0) {
+        #theta_clist[[k]] <- theta_mat[-c(1:warmup),,drop = FALSE]
+        #accept_mat <- accept_mat[-c(1:warmup),, drop=FALSE]
+      }
+      else {
+        #theta_clist[[k]] <- theta_mat
+      }
     }
-    if(warmup > 0) {
-      theta_clist[[k]] <- theta_mat[-c(1:warmup),,drop = FALSE]
-      accept_mat <- accept_mat[-c(1:warmup),, drop=FALSE]
-    }
-    else {
-      theta_clist[[k]] <- theta_mat
-    }
-  }
-  new_dpsim(theta_clist, accept_mat, varnames)
+  theta_clist
+  #new_dpsim(theta_clist, accept_mat, varnames)
 }
 
 
-#' Plot simulation object
-#'
-#' @param object
-#'
 #' @return
 #' @export
 #'
